2f2be567fcc613ef6717e5516ea5be930252995a
Sultan Mahmud Munna <munna.syc@gmail.com>
2026-06-25 19:47:10 +0600

    Type hinting

--- type_hint.py
+++ type_hint.py
@@ -1,9 +1,33 @@
+from typing import Any
+
 text: str = "Rudro"
 
 
+class City:
+    def __init__(self, name: str, population: int) -> None:
+        self.name = name
+        self.population = population
+
+
+Dhaka = City("Dhaka", 8900000)
+
+city_temp: tuple[str, int] = (Dhaka.name, Dhaka.population)
+print(city_temp)
+
 number: int | float = 10.5
 
 number: int | float = 10.5
+
+numbers: list[int | float] = [1, 2.5, 3, 4.5]
+
+fruits: tuple[str, str, str] = ("apple", "banana", "cherry")
+
+
+users: dict[str, Any] = {
+    "name": "Rudro",
+    "age": 25,
+    "is_active": True,
+}
 
 
 def root(num: int | float) -> float:
