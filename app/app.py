84e213ea26401dbdfba3bea7395eb1c32b6ba6af
Meownna <munna.syc@gmail.com>
2026-07-18 18:36:52 +0600

    Refactor get_shipment endpoint to return dynamic shipment details

--- app/app.py
+++ app/app.py
@@ -4,14 +4,23 @@ from typing import Any
 
 app = FastAPI()
 
+# Order of the endpoints matters, the first one will be checked first, so if you have a more specific endpoint, it should be defined before a more generic one.
+@app.get("/shipment/latest")
+def get_latest_shipment() -> dict[str, Any]:
+    return {
+        "shipment_id": 1235,
+        "weight" : 120.65,
+        "shipment_name": "First Shipment",
+        "shipment_status": "In Transit",
+    }
 
 @app.get("/shipment/{id}")
 def get_shipment(id: int) -> dict[str, Any]:
     return {
         "shipment_id": id,
         "weight" : 150.65,
-        "shipment_name": "First Shipment",
-        "shipment_status": "In Transit",
+        "shipment_name": "Random Shipment",
+        "shipment_status": "Delivered",
     }
 
 
