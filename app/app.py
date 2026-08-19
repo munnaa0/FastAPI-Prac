a6942089f56640e6e1c0c65032fa5ad00d2dc529
Meownna <munna.syc@gmail.com>
2026-08-19 23:13:44 +0600

    fix: Update route decorators to include names for home and posts endpoints

--- app/app.py
+++ app/app.py
@@ -161,8 +161,8 @@ posts: list[dict] = [
 ]
 
 
-@app.get("/")
-@app.get("/posts", include_in_schema=False)
+@app.get("/", include_in_schema=False, name="home")
+@app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
     return templates.TemplateResponse(
         request,
