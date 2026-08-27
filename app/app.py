e921538e75f0d45c57ab13dccbd30111ee10ba8b
Meownna <munna.syc@gmail.com>
2026-08-27 22:27:26 +0600

    feat: Add endpoint to retrieve a specific post by ID

--- app/app.py
+++ app/app.py
@@ -174,3 +174,11 @@ def home(request: Request):
 @app.get("/api/posts")
 def return_posts():
     return posts
+
+
+@app.get("/api/posts/{post_id}")
+def retun_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    return {"error": "Post Not FOund"}
