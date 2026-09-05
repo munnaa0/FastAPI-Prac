4f0d622561f154fc46931b5dda5dbdd291dc40af
Meownna <munna.syc@gmail.com>
2026-09-05 19:18:49 +0600

    fix: Correct import order in app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
