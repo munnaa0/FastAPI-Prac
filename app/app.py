cad78933fce70a5ab8530c66fdd4036526cff427
Meownna <munna.syc@gmail.com>
2026-09-07 17:43:56 +0600

    fix: Correct import statements in app.py and update ruff linting configuration in pyproject.toml

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
+from .schemas import PostCreate, PostResponse
 from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
